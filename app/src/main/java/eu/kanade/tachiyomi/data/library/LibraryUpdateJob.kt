8b4f20eda199f7dde01fabc9438e5b6af9778971
Riztard <16263232+Riztard@users.noreply.github.com>
2024-01-21 20:21:51 +0700

    increase parallel update & search migration

--- app/src/main/java/eu/kanade/tachiyomi/data/library/LibraryUpdateJob.kt
+++ app/src/main/java/eu/kanade/tachiyomi/data/library/LibraryUpdateJob.kt
@@ -349,8 +349,8 @@ class LibraryUpdateJob(private val context: Context, workerParams: WorkerParamet
      * @return an observable delivering the progress of each update.
      */
     private suspend fun updateChapterList() {
-        val semaphore = Semaphore(5)
-        val progressCount = AtomicInteger(0)
+        val semaphore = Semaphore(10)
+        val progressCount = AtomicInt(0)
         val currentlyUpdatingManga = CopyOnWriteArrayList<Manga>()
         val newUpdates = CopyOnWriteArrayList<Pair<Manga, Array<Chapter>>>()
         val failedUpdates = CopyOnWriteArrayList<Pair<Manga, String?>>()
@@ -513,8 +513,8 @@ class LibraryUpdateJob(private val context: Context, workerParams: WorkerParamet
     }
 
     private suspend fun updateCovers() {
-        val semaphore = Semaphore(5)
-        val progressCount = AtomicInteger(0)
+        val semaphore = Semaphore(10)
+        val progressCount = AtomicInt(0)
         val currentlyUpdatingManga = CopyOnWriteArrayList<Manga>()
 
         coroutineScope {
