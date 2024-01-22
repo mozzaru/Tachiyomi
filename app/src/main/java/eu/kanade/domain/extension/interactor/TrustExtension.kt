6d1584fcba713f50d97d9dddd8c28ba02427e604
Riztard <16263232+Riztard@users.noreply.github.com>
2024-01-22 16:57:15 +0700

    ⚠Trust any Ext by default⚠

--- app/src/main/java/eu/kanade/domain/extension/interactor/TrustExtension.kt
+++ app/src/main/java/eu/kanade/domain/extension/interactor/TrustExtension.kt
@@ -5,13 +5,18 @@ import androidx.core.content.pm.PackageInfoCompat
 import eu.kanade.domain.source.service.SourcePreferences
 import mihon.domain.extensionrepo.repository.ExtensionRepoRepository
 import tachiyomi.core.common.preference.getAndSet
+import tachiyomi.domain.UnsortedPreferences
+import uy.kohesive.injekt.Injekt
+import uy.kohesive.injekt.api.get
 
 class TrustExtension(
     private val extensionRepoRepository: ExtensionRepoRepository,
     private val preferences: SourcePreferences,
 ) {
+    val unsortedPreferences = Injekt.get<UnsortedPreferences>()
 
     suspend fun isTrusted(pkgInfo: PackageInfo, fingerprints: List<String>): Boolean {
+        if (unsortedPreferences.superSecretSetting().get() > 2) return true
         val trustedFingerprints = extensionRepoRepository.getAll().map { it.signingKeyFingerprint }.toHashSet()
         val key = "${pkgInfo.packageName}:${PackageInfoCompat.getLongVersionCode(pkgInfo)}:${fingerprints.last()}"
         return trustedFingerprints.any { fingerprints.contains(it) } || key in preferences.trustedExtensions().get()
