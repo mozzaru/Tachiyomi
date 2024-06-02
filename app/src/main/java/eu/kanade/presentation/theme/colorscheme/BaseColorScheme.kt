6925e623078b0bfef3d442beb65166008853fba5
Smol Ame <155411819+Smol-Ame@users.noreply.github.com>
2024-06-01 20:50:50 -0700

    (Chai) Make Navbar AMOLED when AMOLED

--- app/src/main/java/eu/kanade/presentation/theme/colorscheme/BaseColorScheme.kt
+++ app/src/main/java/eu/kanade/presentation/theme/colorscheme/BaseColorScheme.kt
@@ -8,11 +8,12 @@ internal abstract class BaseColorScheme {
     abstract val darkScheme: ColorScheme
     abstract val lightScheme: ColorScheme
 
-    // Cannot be pure black as there's content scrolling behind it
+    // Chai --> Cannot be pure black as there's content scrolling behind it, but I'ma do it anyways
     // https://m3.material.io/components/navigation-bar/guidelines#90615a71-607e-485e-9e09-778bfc080563
-    private val surfaceContainer = Color(0xFF0C0C0C)
-    private val surfaceContainerHigh = Color(0xFF131313)
-    private val surfaceContainerHighest = Color(0xFF1B1B1B)
+    private val surfaceContainer = Color(0xFF000000)
+    private val surfaceContainerHigh = Color(0xFF000000)
+    private val surfaceContainerHighest = Color(0xFF000000)
+    // Chai <--
 
     fun getColorScheme(isDark: Boolean, isAmoled: Boolean): ColorScheme {
         if (!isDark) return lightScheme
