229869e482edeb725715479067abc590e89ecddc
AwkwardPeak7 <48650614+AwkwardPeak7@users.noreply.github.com>
2025-07-30 15:12:48 +0500

    Include Manga `initialized` status in backup (#2285)
    (cherry picked from commit 62eec15fe61be88e0ebc8be89a1e445dde55ba7e)
    
    # Conflicts:
    #	app/src/main/java/eu/kanade/tachiyomi/data/backup/create/creators/MangaBackupCreator.kt
    #	app/src/main/java/eu/kanade/tachiyomi/data/backup/models/BackupManga.kt

--- app/src/main/java/eu/kanade/tachiyomi/data/backup/restore/restorers/MangaRestorer.kt
+++ app/src/main/java/eu/kanade/tachiyomi/data/backup/restore/restorers/MangaRestorer.kt
@@ -169,9 +169,7 @@ class MangaRestorer(
         manga: Manga,
     ): Manga {
         return manga.copy(
-            initialized = manga.description != null,
             id = insertManga(manga),
-            version = manga.version,
         )
     }
 
