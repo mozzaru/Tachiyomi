d96db30bd0409ba149d01d48ab5825ebf0008a08
Secozzi <49240133+Secozzi@users.noreply.github.com>
2025-07-30 06:20:43 +0200

    Add option for rendering images in description (#2076)

--- app/src/main/java/eu/kanade/domain/ui/UiPreferences.kt
+++ app/src/main/java/eu/kanade/domain/ui/UiPreferences.kt
@@ -66,6 +66,8 @@ class UiPreferences(
 
     // SY <--
 
+    fun imagesInDescription() = preferenceStore.getBoolean("pref_render_images_description", true)
+
     companion object {
         fun dateFormat(format: String): DateTimeFormatter = when (format) {
             "" -> DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT)
