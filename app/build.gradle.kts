755b4b70cd89eaeae459f82624c59b4eb4ab3e64
mozzaru <rianyanuar52@gmail.com>
2025-08-11 09:39:54 +0700

    Drop support to x86 ABIs; also disable split APKs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,6 +12,8 @@ plugins {
     id("com.github.ben-manes.versions")
 }
 
+val supportedAbis = setOf("armeabi-v7a", "arm64-v8a")
+
 android {
     namespace = "eu.kanade.tachiyomi"
 
@@ -25,6 +27,10 @@ android {
         buildConfigField("String", "COMMIT_SHA", "\"${getGitSha()}\"")
         buildConfigField("String", "BUILD_TIME", "\"${getBuildTime(useLastCommitTime = false)}\"")
         buildConfigField("boolean", "UPDATER_ENABLED", "${Config.enableUpdater}")
+
+        ndk {
+            abiFilters += supportedAbis
+        }
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -91,15 +97,6 @@ android {
     sourceSets {
         getByName("preview").res.srcDirs("src/beta/res")
         getByName("benchmark").res.srcDirs("src/debug/res")
-    }
-
-    splits {
-        abi {
-            isEnable = true
-            isUniversalApk = true
-            reset()
-            include("armeabi-v7a", "arm64-v8a", "x86", "x86_64")
-        }
     }
 
     packaging {
