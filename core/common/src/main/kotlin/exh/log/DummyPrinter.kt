90dabe1bdd9794cfc948c04d6312eb89d02792d4
mozzaru <rianyanuar52@gmail.com>
2025-11-13 15:25:31 +0700

    Revert "fix Dummyprinter"
    This reverts commit f87d7cea7542b982bbb446f13e743ea285baa2b7.

--- core/common/src/main/kotlin/exh/log/DummyPrinter.kt
+++ core/common/src/main/kotlin/exh/log/DummyPrinter.kt
@@ -2,17 +2,15 @@ package exh.log
 
 import com.elvishew.xlog.printer.Printer
 import android.util.Log
+import eu.kanade.tachiyomi.BuildConfig
 
-class DummyPrinter(
-    private val logLevel: Int,
-    private val isDebug: Boolean = false
-) : Printer {
+class DummyPrinter(private val logLevel: Int) : Printer {
     override fun println(logLevel: Int, tag: String?, msg: String?) {
         if (logLevel >= this.logLevel) {
             try {
                 Log.println(logLevel, tag ?: "DummyPrinter", msg ?: "")
             } catch (t: Throwable) {
-                if (isDebug) throw t
+                if (BuildConfig.DEBUG) throw t
             }
         }
     }
