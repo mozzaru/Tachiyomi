0b4de68c2be825d44075ff1a44337846866a306b
mozzaru <rianyanuar52@gmail.com>
2025-11-13 14:14:50 +0700

    Drop support for x86 ABIs and disable split APKs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ plugins {
 
 // shortcutHelper.setFilePath("./shortcuts.xml")
 
-val supportedAbis = setOf("armeabi-v7a", "arm64-v8a", "x86", "x86_64")
+val supportedAbis = setOf("armeabi-v7a", "arm64-v8a")
 
 android {
     namespace = "eu.kanade.tachiyomi"
@@ -36,15 +36,6 @@ android {
             abiFilters += supportedAbis
         }
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-
-    splits {
-        abi {
-            isEnable = true
-            reset()
-            include(*supportedAbis.toTypedArray())
-            isUniversalApk = true
-        }
     }
 
     buildTypes {
