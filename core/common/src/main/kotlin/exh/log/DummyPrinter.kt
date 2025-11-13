f87d7cea7542b982bbb446f13e743ea285baa2b7
mozzaru <rianyanuar52@gmail.com>
2025-11-13 14:56:23 +0700

    fix Dummyprinter

--- core/common/src/main/kotlin/exh/log/DummyPrinter.kt
+++ core/common/src/main/kotlin/exh/log/DummyPrinter.kt
@@ -2,15 +2,17 @@ package exh.log
 
 import com.elvishew.xlog.printer.Printer
 import android.util.Log
-import eu.kanade.tachiyomi.BuildConfig
 
-class DummyPrinter(private val logLevel: Int) : Printer {
+class DummyPrinter(
+    private val logLevel: Int,
+    private val isDebug: Boolean = false
+) : Printer {
     override fun println(logLevel: Int, tag: String?, msg: String?) {
         if (logLevel >= this.logLevel) {
             try {
                 Log.println(logLevel, tag ?: "DummyPrinter", msg ?: "")
             } catch (t: Throwable) {
-                if (BuildConfig.DEBUG) throw t
+                if (isDebug) throw t
             }
         }
     }
